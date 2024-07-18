badd8f1f833ac040b330728bc582507434cb0429
Khushi Topiya <114566494+khushitopiya@users.noreply.github.com>
2024-07-18 17:38:55 +0530

    AreaofCircle.c

---
+++ AreaofCircle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float r,a;
+    
+    printf("enter radius of circle:");
+    scanf("%f",&r);
+    a=3.14*r*r;
+    printf("area of circle is %f",a);
+    getch();
+}
