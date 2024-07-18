8ce706c1234d43904c512622679a870892da651e
Khushi Topiya <114566494+khushitopiya@users.noreply.github.com>
2024-07-18 17:35:54 +0530

    BigSmallNumber.c

---
+++ BigSmallNumber.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(); {
+    int A,B,C;
+    
+    printf("Enter The Number A, B, C: \n");
+    scanf("%d %d %d, &A, &B, &c");
+    
+    if (A >= B && A >= C)
+    printf("%d IsBiggest Number.\n", A);
+    
+    else if(B >= A && B >=C)
+    printf("%d Is Biggest Number.\n", B);
+    
+    else
+        printf("%d Is Biggest Number.\n", C);
+        
+    if (A<=B && A<=C)
+    printf("%d Is Smallest Number.\n", A);
+    
+    else if( B<=A && B<=C)
+    printf("%d Is Smallest Number.", B);
+    
+    else
+    printf("%d Is Smallest Number.", C);
+    
+    retuen 0;
+    
+    
