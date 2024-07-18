b67d4d44ca40c1982eb902725c931e874deafc59
Khushi Topiya <114566494+khushitopiya@users.noreply.github.com>
2024-07-18 17:37:26 +0530

    Cube.c

---
+++ Cube.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    
+    printf("enter a number:");
+    scanf("%d",& number);
+    
+    printf("cube of a number:%d",number*number*number);
+    
+    return 0;
+}
